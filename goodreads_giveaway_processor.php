5509ab20a1035984712051de2ba3555f76705b45
JC Tenney <jc@1010e.net>
2012-06-12 14:06:09 -0600

    Adding debug ability. Fixing class check due to HTML changes in goodreads.

--- goodreads_giveaway_processor.php
+++ goodreads_giveaway_processor.php
@@ -4,6 +4,18 @@ include('./simple_html_dom.php');
 include('./gr_creds.php');
 
 $page = isset($_GET['page']) ? (int)$_GET['page'] : (!empty($argv[1]) ? (int)$argv[1] : NULL);
+$gr_tab = isset($_GET['tab']) ? (int)$_GET['tab'] : (!empty($argv[2]) ? $argv[2] : 'recently_listed');
+if (!empty($gr_debug)) {
+  $folder = './debug-' . date('Y-m-d_H_i_s');
+  mkdir($folder);
+  if (is_dir($folder)) {
+    $gr_debug = $folder . '/';
+    echo "Debug files in: $gr_debug\n";
+  }
+  else {
+    $gr_debug = '';
+  }
+}
 
 if (goodreads_login($gr_un, $gr_pw)) {
   goodreads_process_giveaways($page);
@@ -13,6 +25,7 @@ unlink(getcwd() . '/gr.tmp');
 
 
 function goodreads_login($u, $p) {
+  global $gr_debug;
   static $run = FALSE;
   if ($run) {
     return;
@@ -44,6 +57,9 @@ function goodreads_login($u, $p) {
   $headers[CURLOPT_POST] = TRUE;
   $headers[CURLOPT_POSTFIELDS] = http_build_query($values);
   $output = _goodreads_curl($url, $headers);
+  if ($gr_debug) {
+    file_put_contents($gr_debug . 'login.html', $output);
+  }
   if (!_goodreads_is_logged_in($output)) {
     die ('Failed to login (could not find profile link)');
   }
@@ -66,15 +82,19 @@ function _goodreads_is_logged_in($output) {
 }
 
 function goodreads_process_giveaways($page = NULL) {
+  global $gr_debug, $gr_tab;
   if (!$page) $page = 1;
-  $baseurl = 'http://www.goodreads.com/giveaway?sort=recently_listed&tab=recently_listed';
+  $baseurl = "http://www.goodreads.com/giveaway?sort={$gr_tab}&tab={$gr_tab}";
   $total_processed = $total_found = 0;
   do {
     $processed = $found = 0;
     echo "Page {$page}:";
-    $output = _goodreads_curl($baseurl . '&page=' . $page++);
+    $output = _goodreads_curl($baseurl . '&page=' . $page);
+    if ($gr_debug) {
+      file_put_contents($gr_debug . 'giveawaypage-' . $page . '.html', $output);
+    }
     $html = str_get_html($output);
-    $giveaways = $html->find('div[class=giveaway] a[href^=/giveaway/enter_choose_address]');
+    $giveaways = $html->find('div[class=actions] a[href^=/giveaway/enter_choose_address]');
     $total = count($giveaways);
     echo $total . " giveaways\n";
     foreach ($giveaways as $giveaway) {
@@ -90,6 +110,7 @@ function goodreads_process_giveaways($page = NULL) {
 
     $total_processed += $processed;
     $total_found += $found;
+    $page++;
   } while ($processed);
 
   echo "$total_processed/$total_found";
@@ -97,7 +118,11 @@ function goodreads_process_giveaways($page = NULL) {
 }
 
 function _goodreads_select_address_and_process_giveaway($url) {
+  global $gr_debug;
   $output = _goodreads_curl($url);
+  if ($gr_debug) {
+    file_put_contents($gr_debug . 'choose_address_' . basename($url) . '.html', $output);
+  }
   $html = str_get_html($output);
   $addresses = $html->find('div[id=addresses] a[href^=/giveaway/enter_choose_address]');
 
@@ -109,13 +134,16 @@ function _goodreads_select_address_and_process_giveaway($url) {
   $headers = array();
   $headers[CURLOPT_POST] = TRUE;
   $output = _goodreads_curl(GOODREADS_BASE_URL . $address->href, $headers);
+  if ($gr_debug) {
+    file_put_contents($gr_debug . 'entry_form_' . basename($url) . '.html', $output);
+  }
 
   $html = str_get_html($output);
   $form = $html->find('form[name=entry_form]');
   $form = array_pop($form);
   if (!$form) {
     return FALSE;
-    die('Could not find the sign_in form.');
+    die('Could not find the entry_form form.');
   }
   // Add in the hidden values.
   foreach ($form->find('input[type=hidden]') as $input) {
@@ -128,6 +156,9 @@ function _goodreads_select_address_and_process_giveaway($url) {
 
   // Process actually submitting the final form.
   $output = _goodreads_curl(GOODREADS_BASE_URL . $form->action, $headers);
+  if ($gr_debug) {
+    file_put_contents($gr_debug . 'completed_' . basename($url) . '.html', $output);
+  }
 
   $html = str_get_html($output);
   $processed = $html->find('div[id="header_notice_container"] div[class="noticeBox"]');
